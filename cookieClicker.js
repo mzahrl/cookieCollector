d4057471678ae82a76e9dacfb590e0d099cf1f8f
Mohammed-Michael Zahrl <e1627763@student.tuwien.ac.at>
2021-12-07 13:29:40 +0100

    Added auto cookie collector

---
+++ cookieClicker.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const tmi = require('tmi.js');
+
+const tchannel = '#ThePositiveBot';
+
+const options = {
+    options: {
+        debug: true,
+    },
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+    identity: {
+        username: 'punlshment',
+        password: 'oauth:xl8l1bw4pntzgkkukmd36fwnv3mwlv'
+    },
+    channels: [tchannel]
+};
+
+const client = new tmi.client(options);
+
+client.connect()
+
+client.on('connected', (address, port) => {
+    //get cookie every 2h1m
+    getCookie();
+    setInterval(getCookie, 7260000);
+});
+
+function getCookie() {
+    client.say(tchannel, `!cookie`)
+}
